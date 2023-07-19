7de5981f116164ad2dd0ecf97f335005942eac7f
John Doe <mohaiminur24@gmail.com>
2023-07-19 18:36:35 +0600

    houseData update route is complete

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
+const cors = require("cors");
 var jwt = require("jsonwebtoken");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 3000;
-
 
 app.use(cors());
 app.use(express.json());
-
 
 // Token verify function is here
 const verifyToken = (req, res, next) => {
@@ -31,7 +29,6 @@ const verifyToken = (req, res, next) => {
   });
 };
 
-
 // MongoDb connection from here
 const uri = `mongodb+srv://${process.env.MongodbUserName}:${process.env.MongodbPassword}@cluster0.85env82.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -41,7 +38,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -49,11 +46,10 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-
     // My all of router is from here
     const HouseHunter = client.db("HouseHunter");
-    const users = HouseHunter.collection('users');
-    const AllHouses = HouseHunter.collection('allhouses');
+    const users = HouseHunter.collection("users");
+    const AllHouses = HouseHunter.collection("allhouses");
 
     // verfify House Owner function is here
     const verifyHouseOwner = async (req, res, next) => {
@@ -79,46 +75,67 @@ async function run() {
     };
 
     // get single user houses route is here
-    app.get("/getsingleuserhouses",verifyToken, verifyHouseOwner, async(req,res)=>{
+    app.get(
+      "/getsingleuserhouses",
+      verifyToken,
+      verifyHouseOwner,
+      async (req, res) => {
+        try {
+          const email = req.query.email;
+          const result = await AllHouses.find({ HouseEmail: email }).toArray();
+          res.send(result);
+        } catch (error) {
+          console.log("get single user houses route is not working!");
+        }
+      }
+    );
+
+    //get single house by house owner route is here
+    app.get("/gethouse", verifyToken, verifyHouseOwner, async (req, res) => {
       try {
-        const email = req.query.email;
-        const result = await AllHouses.find({HouseEmail: email}).toArray();
+        const houseID = req.query.id;
+        const result = await AllHouses.findOne({ _id: new ObjectId(houseID) });
         res.send(result);
       } catch (error) {
-        console.log("get single user houses route is not working!")
+        console.log("get single house route is not working");
       }
     });
 
     // new user create route is here
-    app.post('/createnewuser', async(req,res)=>{
-        try {
-          const data = req.body;
-            console.log("body data",data);
-            const availableUser = await users.findOne({email: data.email});
-            console.log(availableUser);
-            if(!availableUser){
-                const result =await users.insertOne(data);
-                res.send(result);
-            }else{
-                res.status(401).send({error: "already available this user", message:"alreadyHaveUser"});
-            };
-        } catch (error) {
-            console.log("create new user route is not working!")
-        };
+    app.post("/createnewuser", async (req, res) => {
+      try {
+        const data = req.body;
+        console.log("body data", data);
+        const availableUser = await users.findOne({ email: data.email });
+        console.log(availableUser);
+        if (!availableUser) {
+          const result = await users.insertOne(data);
+          res.send(result);
+        } else {
+          res
+            .status(401)
+            .send({
+              error: "already available this user",
+              message: "alreadyHaveUser",
+            });
+        }
+      } catch (error) {
+        console.log("create new user route is not working!");
+      }
     });
 
     // get User route is here
-    app.get("/getnewuser", async(req,res)=>{
+    app.get("/getnewuser", async (req, res) => {
       try {
         const UserEmail = req.query.email;
-        const result = await users.findOne({email: UserEmail});
-        if(result){
+        const result = await users.findOne({ email: UserEmail });
+        if (result) {
           res.send(result);
-        }else{
-          res.send({error:"noUser"});
+        } else {
+          res.send({ error: "noUser" });
         }
       } catch (error) {
-        console.log("get user route is not working!")
+        console.log("get user route is not working!");
       }
     });
 
@@ -137,47 +154,60 @@ async function run() {
     });
 
     // delete houses route is here
-    app.delete("/deleteuserroute", verifyToken,verifyHouseOwner, async(req,res)=>{
+    app.delete(
+      "/deleteuserroute",
+      verifyToken,
+      verifyHouseOwner,
+      async (req, res) => {
+        try {
+          const houseid = req.query.id;
+          const result = await AllHouses.deleteOne({
+            _id: new ObjectId(houseid),
+          });
+          res.send(result);
+        } catch (error) {
+          console.log("delete route is not working!");
+        }
+      }
+    );
+
+    // new house create route is here
+    app.post(
+      "/createnewhouse",
+      verifyToken,
+      verifyHouseOwner,
+      async (req, res) => {
+        try {
+          const data = req.body;
+          const result = await AllHouses.insertOne(data);
+          res.send(result);
+        } catch (error) {
+          console.log("create new house route is not working!");
+        }
+      }
+    );
+
+    // house data update by houseowner route is here
+    app.patch("/updatehouse", verifyToken, verifyHouseOwner, async(req, res) => {
       try {
-        const houseid = req.query.id;
-        const result = await AllHouses.deleteOne({_id: new ObjectId(houseid)});
-        res.send(result);
+        const updateID = req.query.id;
+        const updateData = req.body;
+        const query = { _id: new ObjectId(updateID) };
+        const updateDoc = {
+          $set: {...updateData},
+        };
+        const result = await AllHouses.updateOne(query,updateDoc);
+        res.send(result)
       } catch (error) {
-        console.log("delete route is not working!")
+        console.log("update route is not working");
       }
     });
 
-    // new house create route is here
-    app.post("/createnewhouse",verifyToken,verifyHouseOwner,async(req,res)=>{
-      try {
-        const data = req.body;
-        const result = await AllHouses.insertOne(data);
-        res.send(result);
-      } catch (error) {
-        console.log("create new house route is not working!")
-      }
-    })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -185,22 +215,12 @@ async function run() {
 }
 run().catch(console.dir);
 
-
 // Mongodb connectin end from here
 
-
-
-
-
-
-
-
-
-
-app.get("/", (req,res)=>{
-    res.send('HouseHunter server running well');
+app.get("/", (req, res) => {
+  res.send("HouseHunter server running well");
 });
 
-app.listen(port, ()=>{
-    console.log(`House Hunter server running with ${port}`);
-})
+app.listen(port, () => {
+  console.log(`House Hunter server running with ${port}`);
+});
